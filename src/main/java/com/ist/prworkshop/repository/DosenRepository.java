02ab5ef051e085f67b468248f81c0afb32374dfa
Rizki Syahputra <kriiz.sp97@gmail.com>
2021-11-30 10:09:44 +0700

    Added other table repository
    -added DosenRepository for table Dosen
    -added MtkuliahRepository for table Matakuliah

---
+++ src/main/java/com/ist/prworkshop/repository/DosenRepository.java
@@ -0,0 +1,8 @@
+package com.ist.prworkshop.repository;
+
+import com.ist.prworkshop.entity.Dosen;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DosenRepository extends JpaRepository<Dosen, Integer> {
+
+}
---
+++ src/main/java/com/ist/prworkshop/repository/MtkuliahRepository.java
@@ -0,0 +1,8 @@
+package com.ist.prworkshop.repository;
+
+import com.ist.prworkshop.entity.Mtkuliah;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MtkuliahRepository extends JpaRepository<Mtkuliah, Integer> {
+
+}
